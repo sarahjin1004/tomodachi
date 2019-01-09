a9329711e7a3bfaf7bc29035fb1e14b835004a32
root <root@vultr.guest>
2019-01-09 08:45:06 +0000

    updated contact.php

--- contact.php
+++ contact.php
@@ -1,11 +1,3 @@
-<?php include("auth.php"); //include auth.php file on all secure pages ?>
-
-<!--Template Name: vacayhome
-File Name: contact.php
-Author Name: ThemeVault
-Author URI: http://www.themevault.net/
-License URI: http://www.themevault.net/license/-->
-
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -13,7 +5,7 @@ License URI: http://www.themevault.net/license/-->
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="icon" href="images/icons/favicon.png"/>
-        <title>vacayhome</title>
+        <title>Tomodachi</title>
 
         <!-- Bootstrap core CSS -->
         <link href="css/bootstrap.min.css" rel="stylesheet">
@@ -57,8 +49,8 @@ License URI: http://www.themevault.net/license/-->
                             <div class="col-md-2 col-sm-6 col-xs-6">
                                 <div id="logo">
                                     <!--<a href="index.php"><img src="images/logo.png" alt="logo"></a>-->
-                                    <a href="index.php"><span>vacay</span>home</a>
-                                </div>                       
+                                    <a href="index.php"><span>Tomo</span>dachi</a>
+                                </div>
                             </div>
                             <div class="col-sm-6 visible-sm">
                                 <div class="text-right"><button type="button" class="book-now-btn">Book Now</button></div>
@@ -77,18 +69,18 @@ License URI: http://www.themevault.net/license/-->
                                     <div class="collapse navigation navbar-collapse navbar-ex1-collapse remove-space">
                                         <ul class="list-unstyled nav1 cl-effect-10">
                                             <li><a data-hover="Home" href="index.php"><span>Home</span></a></li>
-                                            <li><a data-hover="About" href="about.php"><span>About</span></a></li>
-                                            <li><a data-hover="Rooms" href="rooms.php"><span>Rooms</span></a></li>
-                                            <li><a data-hover="Gallery"  href="gallery.php"><span>Gallery</span></a></li>
-                                            <li><a data-hover="Dinning" href="dinning.php"><span>Dinning</span></a></li>
-                                            <li><a data-hover="News" href="news.php"><span>News</span></a></li>
+                                            <li><a data-hover="Conventions" href="about.php"><span>Conventions</span></a></li>
+                                            <li><a data-hover="Map" href="rooms.php"><span>Map</span></a></li>
+                                            <li><a data-hover="Chat"  href="gallery.php"><span>Chat</span></a></li>
+                                            <li><a data-hover="Megachat" href="dinning.php"><span>Megachat</span></a></li>
+                                            <li><a data-hover="Connect" href="news.php"><span>Connect</span></a></li>
                                             <li><a data-hover="Contact Us" class="active"><span>contact Us</span></a></li>
                                         </ul>
                                     </div>
                                 </nav>
                             </div>
                             <div class="col-md-2  col-sm-4 col-xs-12 hidden-sm">
-                                <div class="text-right"><button type="button" class="book-now-btn">Book Now</button></div>
+                                <div class="text-right"><button type="button" class="book-now-btn"><a href="login.php" style="color:#FFFFFF";>Log in</a></button></div>
                             </div>
                         </div>
                     </div>
@@ -96,12 +88,15 @@ License URI: http://www.themevault.net/license/-->
             </header>
 
 
-            <!--end-->
+            <!--
             <section class="image-head-wrapper" style="background-image: url('images/banner4.jpg');">
+              <br>
+              <h1 style="text-align:center;font-size:60px;color:white;">Who we are</h1><br><br>
+              <p style="text-align:center;color:white;font-size:30px;">We are nonprofit organization that lets community of anime, comic lovers to meet and attend conventions together.</p>
                 <div class="inner-wrapper">
                     <h1>Contact Us</h1>
                 </div>
-            </section>
+            </section>-->
             <div class="clearfix"></div>
 
 
@@ -109,10 +104,10 @@ License URI: http://www.themevault.net/license/-->
                 <div class="container">
                     <div class="col-md-6 contact-left-block">
                         <h3><span>Contact </span>Us</h3>
-                        <p class="text-left">Nulla pharetra eleifend tellus in molestie. In vel neque sit amet urna gravida blandit nec id massa. Phasellus eu aliquet augue. Quisque fringilla urna quam.</p>
-                        <p class="text-right">701 Old York Drive Richmond USA. <i class="fa fa-map-marker fa-lg"></i></p>
-                        <p class="text-right"><a href="tel:+1-202-555-0100"> +1-202-555-0100 <i class="fa fa-phone fa-lg"></i></a></p>
-                        <p class="text-right"><a href="mailto:demo@info.com"> demo@info.com <i class="fa fa-envelope"></i></a></p>
+                        <p class="text-left">Let us know if you have any questions, suggestions, or comments! We listen to our clients!</p>
+                        <p class="text-right">San Francisco Bay Area <i class="fa fa-map-marker fa-lg"></i></p>
+                        <p class="text-right"><a href="tel:(408)805-2688"> (408)805-2688 <i class="fa fa-phone fa-lg"></i></a></p>
+                        <p class="text-right"><a href="mailto:tomodachiconventions@gmail.com"> tomodachiconventions@gmail.com <i class="fa fa-envelope"></i></a></p>
                     </div>
                     <div class="col-md-6 contact-form">
                         <h3>Send a <span>Message</span></h3>
@@ -127,14 +122,14 @@ License URI: http://www.themevault.net/license/-->
                 </div>
             </section>
 
-            <!---map--->
+            <!---map
             <section class="offspace-70">
                 <div class="map">
                     <div class="container">
-                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d101257.12284776446!2d-77.56330202084071!3d37.52477641775529!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89b111095799c9ed%3A0xbfd83e6de2423cc5!2sRichmond%2C+VA%2C+USA!5e0!3m2!1sen!2sin!4v1488891294599"  frameborder="0" style="border:0; width: 100%; height: 400px" allowfullscreen></iframe>
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d101257.12284776446!2d-122.4194!3d37.7749!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89b111095799c9ed%3A0xbfd83e6de2423cc5!2sSan_Francisco%2C+VA%2C+USA!5e0!3m2!1sen!2sin!4v1488891294599"  frameborder="0" style="border:0; width: 100%; height: 400px" allowfullscreen></iframe>
                     </div>
                 </div>
-            </section>
+            </section>-->
             <!---footer--->
             <footer>
                 <div class="container">
@@ -145,20 +140,20 @@ License URI: http://www.themevault.net/license/-->
                                 <ul class="list-unstyled footer-contact-list">
                                     <li>
                                         <i class="fa fa-map-marker fa-lg"></i>
-                                        <p> 701 Old York Drive Richmond USA.</p>
+                                        <p>San Francisco Bay Area</p>
                                     </li>
                                     <li>
                                         <i class="fa fa-phone fa-lg"></i>
-                                        <p><a href="tel:+1-202-555-0100"> +1-202-555-0100</a></p>
+                                        <p><a href="tel:(408)805-2688">(408)805-2688</a></p>
                                     </li>
                                     <li>
                                         <i class="fa fa-envelope-o fa-lg"></i>
-                                        <p><a href="mailto:demo@info.com"> demo@info.com</a></p>
+                                        <p><a href="mailto:tomodachiconventions@gmail.com">tomodachiconventions@gmail.com</a></p>
                                     </li>
                                 </ul>
                                 <div class="footer-social-icon">
                                     <a href="#"><i class="fa fa-facebook"></i></a>
-                                    <a href="#"><i class="fa fa-twitter"></i></a>                           
+                                    <a href="#"><i class="fa fa-twitter"></i></a>
                                     <a href="#"><i class="fa fa-instagram"></i></a>
                                     <a href="#"><i class="fa fa-google-plus"></i></a>
                                     <a href="#"><i class="fa fa-youtube-play"></i></a>
@@ -176,18 +171,18 @@ License URI: http://www.themevault.net/license/-->
                                 <h4>explore</h4>
                                 <ul class="list-unstyled footer-links">
                                     <li><a href="index.php">Home</a></li>
-                                    <li><a href="about.php">About</a></li>
-                                    <li><a href="rooms.php">Rooms</a></li>
-                                    <li><a href="gallery.php">Gallery</a></li>
-                                    <li><a href="#">Dinning</a></li>
-                                    <li> <a href="news.php">News</a></li>
+                                    <li><a href="about.php">Conventions</a></li>
+                                    <li><a href="rooms.php">Map</a></li>
+                                    <li><a href="gallery.php">Chat</a></li>
+                                    <li><a href="#">Megachat</a></li>
+                                    <li> <a href="news.php">Connect</a></li>
                                     <li class="active"> <a href="contact.php">Contact</a></li>
                                 </ul>
                             </div>
                         </div>
                         <div class="col-md-6 col-sm-6 col-xs-12">
                             <div class="footer-details">
-                                <h4>Now On Instagram</h4>
+                                <h4>Coming Soon: Mobile Applications</h4>
                                 <div class="row">
                                     <div class="instagram-images">
                                         <div id="instafeed"></div>
@@ -198,7 +193,7 @@ License URI: http://www.themevault.net/license/-->
                     </div>
 
                     <div class="copyright">
-                        &copy; 2017 All right reserved. Designed by <a href="http://www.themevault.net/" target="_blank">ThemeVault.</a>
+                        &copy; 2017 All right reserved.
                     </div>
 
                 </div>
